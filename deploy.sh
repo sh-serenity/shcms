c2edd3f347227e9524f6278b6cc4081da65607b7
root <root@d3.shushik.net>
2022-05-31 03:56:24 +0200

    d

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 buildnumber=`cat ../buildnumber`
-wget http://dev.shushik.kiev.ua/Dockerfile 2>&1>/build/log
+wget -O ./Dockerfile http://dev.shushik.kiev.ua/d.sh 2>&1>/build/log
 url=`cat ../github`
 sed -i 's+_GIT_HUB_+$url+g' Dockerfile
 docker build . -t stormstack/bshcms:$buildnumber 2>&1>/build/log
