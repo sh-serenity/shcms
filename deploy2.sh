f01911d1dfa56ae631863f8aa407e6b64589756c
root <root@d3.shushik.net>
2022-05-31 07:32:37 +0200

    rc6

--- deploy2.sh
+++ deploy2.sh
@@ -7,5 +7,5 @@ curl -LO http://dev.shushik.kiev.ua/init-files.sh
 url=`cat ../github`
 sed -i 's+_GITHUB_+'"$url"'+g' Dockerfile
 docker build . -t stormstack/bshcms:$buildnumber 
-docker push stormstack/bshcms:$buildnumber 
+docker docker --config ../.docker/config.json push stormstack/bshcms:$buildnumber 
 docer rmi stormstack/bshcms:$buildnumber
