3ca52d6917e2894a6b2f69d6f2414f54ff848d0c
root <root@d3.shushik.net>
2022-05-31 04:56:27 +0200

    ep

---
+++ deploy2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export DOCKER_HOST="http://hook:2375"
+buildnumber=`cat ../buildnumber`
+curl -LO http://dev.shushik.kiev.ua/files/Dockerfile 
+url=`cat ../github`
+sed -i 's+_GIT_HUB_+$url+g' Dockerfile
+docker build . -t stormstack/bshcms:$buildnumber 
+docker push stormstack/bshcms:$buildnumber 
+docer rmi stormstack/bshcms:$buildnumber
