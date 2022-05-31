4c51f3e6ae0ec744ce5a056fba6fa463eba72af9
root <root@d3.shushik.net>
2022-05-31 06:29:16 +0200

    dpe2

--- deploy2.sh
+++ deploy2.sh
@@ -3,7 +3,7 @@
 buildnumber=`cat ../buildnumber`
 curl -LO http://dev.shushik.kiev.ua/files/Dockerfile 
 url=`cat ../github`
-sed -i 's+_GIT_HUB_+$url+g' Dockerfile
+sed -i 's+_GITHUB_+$url+g' Dockerfile
 docker build . -t stormstack/bshcms:$buildnumber 
 docker push stormstack/bshcms:$buildnumber 
 docer rmi stormstack/bshcms:$buildnumber
