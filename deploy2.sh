e09080acc0dda461524f6a52fcb4a6d4a64d9d5c
root <root@d3.shushik.net>
2022-05-31 06:57:09 +0200

    init-files

--- deploy2.sh
+++ deploy2.sh
@@ -2,7 +2,8 @@
 
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 buildnumber=`cat ../buildnumber`
-curl -LO http://dev.shushik.kiev.ua/files/Dockerfile 
+curl -LO http://dev.shushik.kiev.ua/files/Dockerfile
+curl -LO http://dev.shushik.kiev.ua/init-files.sh
 url=`cat ../github`
 sed -i 's+_GITHUB_+$url+g' Dockerfile
 docker build . -t stormstack/bshcms:$buildnumber 
